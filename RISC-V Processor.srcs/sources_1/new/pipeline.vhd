00d5bb05d0867c84d7430f9afde3c7212bf6dcb0
tomeh2 <tomislav.harmina@gmail.com>
2021-03-21 19:45:14 +0100

    Added execute stage to pipeline

--- RISC-V Processor.srcs/sources_1/new/pipeline.vhd
+++ RISC-V Processor.srcs/sources_1/new/pipeline.vhd
@@ -43,6 +43,11 @@ architecture rtl of pipeline is
     signal dec_reg_data_1_out, dec_reg_data_2_out : std_logic_vector(31 downto 0);
     signal dec_alu_op_out : std_logic_vector(3 downto 0);
     
+    -- EXECUTE STAGE SIGNALS
+    signal exe_reg_data_bus_1_in, exe_reg_data_bus_2_in : std_logic_vector(31 downto 0);
+    signal exe_alu_res_out : std_logic_vector(31 downto 0);
+    signal exe_alu_op_in : std_logic_vector(3 downto 0);
+    
     -- GLOBAL CONTROL SIGNALS
     signal clk, reset : std_logic;
     
@@ -56,15 +61,26 @@ begin
                             alu_op => dec_alu_op_out,
                             clk => clk,
                             reset => reset);
+                            
+    stage_execute : entity work.stage_execute(arch)
+                    port map(reg_data_bus_1 => exe_reg_data_bus_1_in,
+                             reg_data_bus_2 => exe_reg_data_bus_2_in,
+                             alu_res_bus => exe_alu_res_out,
+                             alu_op => exe_alu_op_in);
     
     -- ============ PIPELINE REGISTER INITIALIZATIONS ============
     reg_de : entity work.register_var
              generic map(WIDTH_BITS => 68)
-                      -- Datapath data signals
+                      -- Datapath data signals in
              port map(d(31 downto 0) => dec_reg_data_1_out,
                       d(63 downto 32) => dec_reg_data_2_out,
-                      -- Datapath control signals
+                      -- Datapath control signals in
                       d(67 downto 64) => dec_alu_op_out,
+                      -- Datapath data signals out
+                      q(31 downto 0) => exe_reg_data_bus_1_in,
+                      q(63 downto 32) => exe_reg_data_bus_2_in,
+                      -- Datapath control signals out
+                      q(67 downto 64) => exe_alu_op_in,
                       -- Register control
                       clk => clk,
                       reset => reset,
